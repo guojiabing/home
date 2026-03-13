c430c02e78ce5347c09eefdaa80e5db198a17d2b
guojiabing <guo_jiabing@foxmail.com>
2026-03-13 23:00:57 +0800

    feat: Add new music tracks and update API endpoints.

--- src/api/index.js
+++ src/api/index.js
@@ -1,42 +1,27 @@
 // import axios from "axios";
-import fetchJsonp from "fetch-jsonp";
 
 /**
  * 音乐播放器
  */
 
-// 获取音乐播放列表
-export const getPlayerList = async (server, type, id) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
-  );
-  const data = await res.json();
-
-  if (data[0].url.startsWith("@")) {
-    // eslint-disable-next-line no-unused-vars
-    const [handle, jsonpCallback, jsonpCallbackFunction, url] = data[0].url.split("@").slice(1);
-    const jsonpData = await fetchJsonp(url).then((res) => res.json());
-    const domain = (
-      jsonpData.req_0.data.sip.find((i) => !i.startsWith("http://ws")) ||
-      jsonpData.req_0.data.sip[0]
-    ).replace("http://", "https://");
-
-    return data.map((v, i) => ({
-      name: v.name || v.title,
-      artist: v.artist || v.author,
-      url: domain + jsonpData.req_0.data.midurlinfo[i].purl,
-      cover: v.cover || v.pic,
-      lrc: v.lrc,
-    }));
-  } else {
-    return data.map((v) => ({
-      name: v.name || v.title,
-      artist: v.artist || v.author,
-      url: v.url,
-      cover: v.cover || v.pic,
-      lrc: v.lrc,
-    }));
-  }
+// 获取音乐播放列表 (使用纯静态自闭环，彻底告别所有防盗链和跨域封禁)
+export const getPlayerList = async () => {
+  return [
+    {
+      name: "起风了",
+      artist: "买辣椒也用券",
+      url: "/music/qifengle.mp3",
+      cover: "/images/background10.jpg",
+      lrc: "[00:00.00] 歌词未内置"
+    },
+    {
+      name: "海阔天空",
+      artist: "Beyond",
+      url: "/music/haikuotiankong.mp3",
+      cover: "/images/background1.jpg",
+      lrc: "[00:00.00] 歌词未内置"
+    }
+  ];
 };
 
 /**
